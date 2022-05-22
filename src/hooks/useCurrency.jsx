87976fbd7ab2d546300b2306fbe90fa1dd65e83e
braz99 <fabriciobrazil99@gmail.com>
2022-05-22 11:55:56 -0300

    Cria variáveis de ambiente para guardar a apiKey

--- src/hooks/useCurrency.jsx
+++ src/hooks/useCurrency.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
 export default function useCurrency(from, to) {
-  
   let [PriceCurrency, setPriceCurrency] = useState(0.0);
   let [exchangedCurrency, setExchangedCurrency] = useState(0);
 
   let data = `${from}_${to}`;
-  let url = `https://free.currconv.com/api/v7/convert?q=${data}&compact=ultra&apiKey=c0e646585b759fbb1ea0`;
+  let apiKey = process.env.REACT_APP_API_KEY;
+  let url = `https://free.currconv.com/api/v7/convert?q=${data}&compact=ultra&apiKey=${apiKey}`;
+
+  let currencies = `https://free.currconv.com/api/v7/currencies?apiKey=${apiKey}`;
 
   function exchange() {
     fetch(url)
